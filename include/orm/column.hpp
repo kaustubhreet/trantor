7ce5bdec8573a4cb943781911235f1b33fe53720
verma-kartik <kartikv.vero058@gmail.com>
2023-07-12 15:31:17 +0530

    added constraints into columns

---
+++ include/orm/column.hpp
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <common.hpp>
+#include <sstream>
+#include "types.hpp"
+
+namespace trantor{
+    template <trantor::FixedLengthString columnName, auto Getter, auto Setter= nullptr, class... Constraint>
+    class ColumnP{
+    private:
+        template<typename T>
+        struct resolve_function_ptr_types : std::false_type {
+            using argType = std::false_type;
+            using returnType = void;
+            using klass = std::false_type;
+        };
+
+        //pointer to setter
+        template<typename C, class A>
+        struct resolve_function_ptr_types<void (C::*)(A)> {
+            using argType = A;
+            using returnType = void;
+            using klass = C;
+        };
+
+        //pointer to getter
+        template<typename R, typename C>
+        struct resolve_function_ptr_types<R (C::*)()>
+        {
+            using argType = std::false_type;
+            using returnType = R;
+            using klass = C;
+        };
+
+        //pointer to member
+        template<typename R, typename C>
+        struct resolve_function_ptr_types<R C::*>
+        {
+            using argType = std::false_type;
+            using returnType = void;
+            using klass = std::false_type;
+        };
+
+        using SetterResolved = resolve_function_ptr_types<decltype(Setter)>;
+        using GetterResolved = resolve_function_ptr_types<decltype(Getter)>;
+
+        static_assert(!std::is_same<typename SetterResolved::argType, std::false_type>::value,
+                      "Column template argument should be a pointer to a class method that sets the column data");
+        static_assert(std::is_same<typename SetterResolved::returnType, void>::value,
+                      "Column template argument should be a pointer to a class method that sets the column data. The return type should be `void`");
+
+        static_assert(std::is_same<typename GetterResolved::argType, std::false_type>::value,
+                      "Column template argument should be a pointer to a class method that gets the column data");
+        static_assert(!std::is_same<typename GetterResolved::returnType, void>::value,
+                      "Column template argument should be a pointer to a class method that gets the column data. The return type should not be `void`");
+
+        static_assert(std::is_same<typename GetterResolved::returnType, typename SetterResolved::argType>::value,
+                      "Column template arguments should be a pointers to class methods that get and set the column data");
+
+    public:
+
+        using MemberType = SetterResolved::argType;
+        using ObjectClass = SetterResolved::klass;
+
+        static constexpr const char* name(){
+            return columnName.value;
+        }
+
+        static constexpr trantor::sql_type_t SQLMemberType = trantor::MemberTypeToSqlType<MemberType>::value;
+
+        static auto getter(auto obj){
+            return (obj.*Getter)();
+        };
+
+        static void setter(auto obj, auto arg){
+            obj.*Setter(arg);
+        };
+
+        static std::string creationConstraints() {
+            std::stringstream ss;
+            ([&]{
+                ss << Constraint::query() << ",";
+            }(),...);
+
+            std::string qstr = ss.str();
+
+            if(qstr.size() >= 2)    qstr.erase(qstr.end() - 1);
+
+            return qstr;
+        }
+
+
+    };
+
+
+    template <trantor::FixedLengthString columnName, auto M, class... Constraint>
+    class Column{
+    private:
+        template<typename T>
+        struct find_column_type : std::false_type {
+            using type = std::false_type;
+        };
+
+        template<typename R, typename C, class A>
+        struct find_column_type<R (C::*)(A)>
+        {
+            using type = std::false_type;
+        };
+
+        template<typename R, typename C>
+        struct find_column_type<R C::*>
+        {
+            using type = R;
+            using klass = C;
+        };
+
+    public:
+
+        using MemberType = find_column_type<decltype(M)>::type;
+
+        static_assert(!std::is_same<MemberType, std::false_type>::value,
+                      "Column template argument should be a pointer to a class memebr");
+
+        using ObjectClass = find_column_type<decltype(M)>::klass;
+
+        static constexpr const char* name(){
+            return columnName.value;
+        }
+
+        static auto getter(auto obj){
+            return obj.*M;
+        };
+
+        static void setter(auto obj, auto arg){
+            obj.*M = arg;
+        };
+
+        static std::string creationConstraints() {
+            std::stringstream ss;
+            ([&]{
+                ss << Constraint::to_string() << ",";
+            }(),...);
+
+            std::string qstr = ss.str();
+
+            if(qstr.size() >= 2)    qstr.erase(qstr.end() - 1);
+
+            return qstr;
+        }
+
+
+        static constexpr trantor::sql_type_t SQLMemberType = trantor::MemberTypeToSqlType<MemberType>::value;
+    };
+}
--- include/orm/constraint.hpp
+++ include/orm/constraint.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "common.hpp"
+#include <string>
+#include <sstream>
 
 namespace trantor{
     enum class conflict_t {
@@ -32,5 +34,49 @@ namespace trantor{
             return "unknown conflict";
 
         }
+    }
+
+    namespace column_constraint{
+        template<FixedLengthString value>
+        struct Default{
+            static const std::string to_string(){
+                std::stringstream ss;
+                ss << "DEFAULT '" << value.value << "'";
+                return ss.str();
+            }
+        };
+
+        template<FixedLengthString value>
+        struct Collate{
+            static const std::string to_string(){
+                std::stringstream ss;
+                ss << "COLLATE '" << value.value << "'";
+                return ss.str();
+            }
+        };
+
+        template<FixedLengthString constraint, conflict_t onConflict>
+        struct ConstraintWithConflictClause{
+            static std::string to_string(){
+                std::stringstream ss;
+                ss << constraint.value << "ON CONFLICT" << conflictStr(onConflict);
+                return ss.str();
+            }
+        };
+
+        template<conflict_t onConflict=conflict_t::ABORT>
+        using Unique = ConstraintWithConflictClause<"UNIQUE", onConflict>;
+
+        template<conflict_t onConflict=conflict_t::ABORT>
+        using NotNull = ConstraintWithConflictClause<"NOT NULL", onConflict>;
+
+        template<conflict_t onConflict=conflict_t::ABORT>
+        using PrimaryKey = ConstraintWithConflictClause<"PRIMARY KEY", onConflict>;
+
+        template<conflict_t onConflict=conflict_t::ABORT>
+        using PrimaryKeyAsc = ConstraintWithConflictClause<"PRIMARY KEY ASC", onConflict>;
+
+        template<conflict_t onConflict=conflict_t::ABORT>
+        using PrimaryKeyDesc = ConstraintWithConflictClause<"PRIMARY KEY DESC", onConflict>;
     }
 }
