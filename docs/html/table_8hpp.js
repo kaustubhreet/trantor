0752ff4daf96de88ef122bbb6492ea6f7b5c256c
verma-kartik <kartikv.vero058@gmail.com>
2023-08-22 01:31:21 +0530

    docs update

---
+++ docs/html/table_8hpp.js
@@ -0,0 +1,5 @@
+var table_8hpp =
+[
+    [ "trantor::Table< tableName, T, Column >", "classtrantor_1_1_table.html", null ],
+    [ "trantor::ColumnBelongingToClass", "concepttrantor_1_1_column_belonging_to_class.html", null ]
+];
